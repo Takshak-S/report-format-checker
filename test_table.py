8d25a9c5f84907370ba3dd0eb95dde4279b59cd2
Takshak-S <takshak.s2006@gmail.com>
2026-08-17 04:21:42 +0000

    chore: remove debug audit files and test_table.py, add audit_*.json to .gitignore

--- test_table.py
+++
@@ -1,9 +0,0 @@
-import sys
-import pandas as pd
-from nlp.dom import DocumentModel
-from services.table_service import get_table_metadata
-import pickle
-
-with open("test.pkl", "rb") as f:
-    pass
-
